a4c1eaecdf3de3cffac11c9fdf188b9d3fbacf21
Rathi <github email address>
2017-04-18 16:01:28 -0400

    currently working

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,2 +1,11 @@
 class V1::SessionsController < ApplicationController
+  skip_before_action :authenticate
+  def create
+
+
+  end
+  private
+  def account_params
+    params.require(:account).permit(:username, :password)
+  end
 end
