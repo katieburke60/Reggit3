56759f913859cefde2c74ec91c4196fd53e0169a
Rathi <github email address>
2017-04-23 13:31:36 -0400

    working on  vote/comment controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -15,4 +15,5 @@ class VotesController < ApplicationController
     redirect_to root_path
   end
 
+
 end
---
+++ app/serializers/vote_serializer.rb
@@ -0,0 +1,4 @@
+class VoteSerializer < ActiveModel::Serializer
+  attributes :id, :vote
+
+end
