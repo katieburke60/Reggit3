4fd9ed99f7f42ac34c44d7e975994fa2180f73d8
Rathi <github email address>
2017-04-24 15:38:51 -0400

    votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,33 +1,11 @@
+require 'pry'
+
 class VotesController < ApplicationController
 
   def create
-
-     @regulation = Regulation.find(params[:regulation_id])
-     @allvotesForRegulation = @regulation.votes
-     @specificVoteInstance = Votes.find(params[:id])
-     @specificVoteInstance.update(vote_params)
-
-     render json: regulation.votes
-
+     Vote.create(vote: params['vote'], regulation_id: params['regulation_id'])
   end
 
-  def vote_up
-    @regulation= Regulation.find(params[:id])
-    @vote = @regualtion.vote
-    @regulation.update_attribute(:votes_up, @regulation.vote = "up")
 
-  end
 
-  def vote_down
-    @regulation = Regulation.find(params[:id])
-    @vote = @regulation.vote
-    @regulation.update_attribute(:votes_down, @regulation.vote = "down")
-
-  end
- private
- def vote_params
-
-   params.require(:vote).permit(:regulation_id, :user_id)
-
- end
 end
