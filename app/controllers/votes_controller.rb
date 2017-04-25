292c3258652e73afe21a3b15624ff1ca945e7547
Rathi <github email address>
2017-04-25 16:29:23 -0400

    fixed vote controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,11 @@
 require 'pry'
-
 class VotesController < ApplicationController
 
   def create
-     Vote.create(vote: params['vote'], regulation_id: params['regulation_id'])
+    regulation = Regulation.find(params[:regulation_id])
+     vote = regulation.votes.create(vote: params['vote'])
+    #  Vote.create(vote: params['vote'], regulation_id: params['regulation_id'])
+     render json: regulation.votes
   end
-
-
 
 end
