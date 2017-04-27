ce576fbdd1c31585804cc511efd153cebbf67296
Rathi <github email address>
2017-04-27 00:06:42 -0400

    worked on vote controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,54 +1,18 @@
+require 'pry'
 class CommentsController < ApplicationController
 
   def create
-     Comment.create(comment: params['comment'], regulation_id: params['regulation_id'])
+  regulation = Regulation.find(params[:regulation_id])
+  comment = regulation.comments.create(comment)
+  comment.save
+  render json: comment
+end
+
+private
+
+  def comment_params
+    params.require(:comment).permit(:description, :regulation_id )
   end
-  
-  # def index
-  #   @comments = Comment.all
-  # end
-  #
-  # def show
-  #   @comment = Comment.find(params[:id])
-  #   respond_to do |format|
-  #     format.html { render :show }
-  #     format.json { render json: @comment.to_json(only: [ :description, :id] ) }
-  #
-  #   end
-  # end
 
-  # def new
-  #   @comment = Comment.new
-  # end
 
-  # def create
-  #   @comment = Post.create(comment_params)
-  #   @comment.save
-  #   redirect_to comment_path(@comment)
-  # end
-
-  # def edit
-  # end
-  #
-  # def update
-  #   @comment.update(comment_params)
-  #   redirect_to comment_path(@comment)
-  # end
-
-#   def comment_data
-#     comment = Comment.find(params[:id])
-#     #render json: PostSerializer.serialize(post)
-#     render json: comment.to_json(only: [:description, :id],)
-#   end
-#
-# private
-#   # Use callbacks to share common setup or constraints between actions.
-#   def set_comment
-#     @comment = Comment.find(params[:id])
-#   end
-#
-#   # Never trust parameters from the scary internet, only allow the white list through.
-#   def comment_params
-#     params.require(:comment).permit( :description)
-#   end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,11 +1,17 @@
 require 'pry'
 class VotesController < ApplicationController
+     def create
 
-  def create
-    regulation = Regulation.find(params[:regulation_id])
-     vote = regulation.votes.create(vote: params['vote'])
-    #  Vote.create(vote: params['vote'], regulation_id: params['regulation_id'])
-     render json: regulation.votes
-  end
+       regulation = Regulation.find(params[:regulation_id])
+       vote = regulation.votes.create(vote)
+       vote.save
+       #  Vote.create(vote: params['vote'], regulation_id: params['regulation_id'])
+       render json: regulation.votes
+      end
+
+private
+def vote_params
+  params.require(:vote).permit(:vote, :regulation_id)
+end
 
 end
